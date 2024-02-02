dc55d92676ea980ce8cc9b97a083905b455b97b0
Arcange <VIM-Arcange@users.noreply.github.com>
2024-02-02 20:16:50 +0100

    New translations en.js (German)

--- src/i18n/de.js
+++ src/i18n/de.js
@@ -1,128 +1,128 @@
 // This is just an example,
 // so you can safely delete all default props below
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful',
+  failed: false,
+  success: false,
   menu: {
-    accounts: 'Accounts',
-    about: 'About',
-    lock: 'Lock application',
-    logs: 'Logs'
+    accounts: false,
+    about: false,
+    lock: false,
+    logs: false
   },
   logs: {
-    success_copy_clipboard: 'Data copied to clipboard',
-    error_copy_clipboard: 'Failed to copy data to clipboard'
+    success_copy_clipboard: false,
+    error_copy_clipboard: false
   },
   scan: {
-    error_scan: 'QR scan failed',
-    error_permission_denied: 'Camera Permissions denied',
-    error_start_scan: 'Permissions denied or Error when getting camera permissions'
+    error_scan: false,
+    error_permission_denied: false,
+    error_start_scan: false
   },
   login: {
-    pin_create: 'Choose your PIN code',
-    pin_confirm: 'Confirm your PIN code',
-    pin_enter: 'Enter your PIN code',
-    pin_mismatch: 'PIN codes do not match!',
-    pin_forgotten: 'Forgotten pin code?',
-    pin_init: 'PIN code initialized',
-    pin_reset: 'Reset done',
-    pin_error: 'Error initializing PIN code',
-    failed: 'Authentication failed',
-    biometrics_unavailable: 'Device Biometrics not available. Please enable them and restart the app.',
+    pin_create: false,
+    pin_confirm: false,
+    pin_enter: false,
+    pin_mismatch: false,
+    pin_forgotten: false,
+    pin_init: false,
+    pin_reset: false,
+    pin_error: false,
+    failed: false,
+    biometrics_unavailable: false,
     confirm_reset: {
-      title: 'Confirm PIN reset',
-      message: '<span class="text-red"><b>WARNING:</b></span> If you reset your PIN, all data including keys and authentications will be cleared.<br>You will then need to re-import your keys.'
+      title: false,
+      message: false
     }
   },
   store_auth: {
-    biometrics_reason: 'Allow HiveAuth to use your biometrics to Authenticate',
-    biometrics_title: 'Authenticate',
-    biometrics_subtitle: '',
+    biometrics_reason: false,
+    biometrics_title: false,
+    biometrics_subtitle: false,
     //"Unlock HiveAuth with your Biometrics",
-    biometrics_description: 'Provide your FaceID or Touch ID to unlock HiveAuth'
+    biometrics_description: false
   },
   accounts: {
-    btn_add: 'Add an Account',
-    empty_sessions: 'No sessions found',
-    keys: 'Keys',
-    sessions: 'Sessions'
+    btn_add: false,
+    empty_sessions: false,
+    keys: false,
+    sessions: false
   },
   accounts_key: {
     confirm_delete_key: {
-      title: 'Remove Key',
-      message: 'Are you sure you want to remove this key?'
+      title: false,
+      message: false
     },
     confirm_delete_account: {
-      title: 'Remove Account',
-      message: 'Removing that key will also remove the account. Do you want to continue?'
+      title: false,
+      message: false
     },
-    account_deleted: 'The account has been removed',
-    key_deleted: 'The key has been removed'
+    account_deleted: false,
+    key_deleted: false
   },
   account_session: {
-    expires: 'Expires',
-    empty: 'No sessions found',
+    expires: false,
+    empty: false,
     confirm_delete_session: {
-      title: 'Delete session',
-      message: 'Are you sure you want to delete this session?'
+      title: false,
+      message: false
     },
     whilelists: {
-      title: 'Whitelisted operations',
-      deleted: 'Operation whitelist removed'
+      title: false,
+      deleted: false
     }
   },
   dialog_auth_req: {
-    text: 'wants to authenticate on',
-    timeout: 'Session validity'
+    text: false,
+    timeout: false
   },
-  dialog_challenge_req: { text: 'wants to validate a challenge' },
+  dialog_challenge_req: { text: false },
   dialog_sign_req: {
-    text: 'wants to sign a transaction with the following operation(s):',
-    whitelist: 'Do not prompt again for {type} operations'
+    text: false,
+    whitelist: false
   },
   import_key: {
     title: {
-      account: 'Import your Hive account and key',
-      key: 'Import your account key'
+      account: false,
+      key: false
     },
-    success: 'Key is securely stored',
-    failed: 'Failed to validate key',
-    no_match: 'No matching key',
-    error_owner_key: 'Importing an owner key is not allowed',
-    invalid: 'Invalid {type} Key',
-    invalid_keychain_export: 'The keychain export contains an invalid key',
+    success: false,
+    failed: false,
+    no_match: false,
+    error_owner_key: false,
+    invalid: false,
+    invalid_keychain_export: false,
     username: {
-      label: 'Username',
-      placeholder: 'Enter your Hive username'
+      label: false,
+      placeholder: false
     },
     key: {
-      label: 'Private {type} Key',
-      placeholder: 'Enter or scan your private key'
+      label: false,
+      placeholder: false
     },
-    btn_import: 'Import'
+    btn_import: false
   },
   operation_details: {
-    pretty: 'Pretty',
-    raw: 'Raw'
+    pretty: false,
+    raw: false
   },
-  main_page: { no_pending_request: 'No pending request' },
+  main_page: { no_pending_request: false },
   main_layout: {
-    connected: 'connected',
-    disconnected: 'disconnected',
-    registered: ' registered',
-    auth_ack: 'Authentication approved',
-    sign_ack: 'Transaction signed',
-    challenge_ack: 'Challenge signed',
-    auth_not_found: 'No valid authentication found',
-    account_not_found: 'Account {name} is not managed'
+    connected: false,
+    disconnected: false,
+    registered: false,
+    auth_ack: false,
+    sign_ack: false,
+    challenge_ack: false,
+    auth_not_found: false,
+    account_not_found: false
   },
-  btn_ok: 'OK',
-  btn_cancel: 'Cancel',
-  btn_add: 'Add',
-  btn_save: 'Save',
-  btn_update: 'Update',
-  btn_approve: 'Approve',
-  btn_reject: 'Reject',
-  not_supported: 'Feature not supported yet',
-  request_expired: 'Request expired'
+  btn_ok: false,
+  btn_cancel: false,
+  btn_add: false,
+  btn_save: false,
+  btn_update: false,
+  btn_approve: false,
+  btn_reject: false,
+  not_supported: false,
+  request_expired: false
 };
